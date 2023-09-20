c9a62abec87328688737ffb265823a328d5ff3d2
vib3sf <vib3sf@proton.me>
2023-09-20 09:19:20 +0300

    handle cd command as chdir syscall

--- exec.c
+++ exec.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int exec(char **argv)
 {
@@ -15,6 +16,22 @@ int exec(char **argv)
 
 	if(p == 0) 
 	{
+		if(!strcmp(argv[0], "cd")) 
+		{
+			if (argv[2])
+			{
+				printf("Error: cd have only 1 arg");
+				return -1;
+			}
+
+			int chd = chdir(argv[1]);
+			if (chd == -1) 
+			{
+				perror(argv[1]);
+				return -1;
+			}
+			return 0;
+		}
 		execvp(argv[0], argv);
 		perror(argv[0]);
 		return -1;
