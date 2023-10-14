0dd29d51ca75925dc07b1fa7152796118f2f7ccb
vib3sf <vib3sf@proton.me>
2023-10-14 23:38:38 +0300

    rm main obj from deps

--- deps.mk
+++ deps.mk
@@ -1,6 +1,5 @@
 command.o: src/command.c src/command.h src/exec.h src/stream.h
 exec.o: src/exec.c src/exec.h src/command.h src/stream.h src/sig.h
-main.o: src/main.c src/command.h src/parse.h src/exec.h src/sig.h
 parse.o: src/parse.c src/parse.h
 sig.o: src/sig.c src/sig.h
 stream.o: src/stream.c src/stream.h
