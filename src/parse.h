c08a93b79a158295d82a089aac26a4e0976859aa
vib3sf <vib3sf@proton.me>
2023-10-31 20:18:06 +0300

    update parse doc

--- src/parse.h
+++ src/parse.h
@@ -1,19 +1,29 @@
 #ifndef PARSE_H
 #define PARSE_H
 
+/* all types of args are listed here */
+
 typedef enum arg_type
 {
+	/* usual arg */
 	word_arg,
+
+	/* && and || args */
 	logical_and_arg,
 	logical_or_arg,
+
+	/* arg whih indicates exec in bg mode */
 	bg_arg,
 	pipe_arg,
+
+	/* stream redirectioin args */
 	in_arg,
 	out_arg,
 	append_arg,
 }
 arg_type_t;
 
+/* command node */
 typedef struct argument
 {
 	char *s;
@@ -23,9 +33,9 @@ argument_t;
 
 typedef enum parse_res
 {
+	no_err,
 	quotes_err,
 	special_char_err,
-	no_err,
 	empty_cmd,
 }
 parse_res_t;
@@ -43,22 +53,28 @@ typedef enum char_state
 	special,
 	quotes,
 	space
-} char_state_t;
+} 
+char_state_t;
 
 typedef struct parse
 {
+	/* reading char data */
 	int c;
 	char_state_t state;
+
+	/* buffer for parsing commands */
 	char *buf;
 	int buf_size;
 	int buf_count;
+
+	/* result of parsing */
 	argument_t *argv;
 	int argc;
 	parse_res_t res;
-} parse_t;
+} 
+parse_t;
 
 int parse_command(argument_t **argv_buf, int *argc);
 void free_argv(argument_t *argv, int argc);
-
 
 #endif
